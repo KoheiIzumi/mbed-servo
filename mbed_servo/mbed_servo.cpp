f8b5a7010f286bece34b8b0c5a8f6f8ddd61e825
Kohei Izumi <kohei.izumi@gmail.com>
2014-06-05 02:20:26 +0900

    Change send packet and operation of the servo with specified time.

--- mbed_servo/mbed_servo.cpp
+++ mbed_servo/mbed_servo.cpp
@@ -11,22 +11,22 @@ Serial device(p9, p10);         // tx = P9, rx = P10
 DigitalOut REDE(p11);           // RS485 Transmit Enable
 /*--------------------------------------------------*/
 /* Funcyion     : mbed initialize                   */
-/* NAME         : init                              */
+/* NAME         : Init                              */
 /* Argument     : ---                               */
 /* Return value : ---                               */
 /*--------------------------------------------------*/
-void init(void){
+void Init(void){
     device.baud(115200);        // baud Rate = 115.2kbps [Futaba default]
     REDE = 0;                   // RS485 Transmit disable
 }
 /*--------------------------------------------------*/
-/* Funcyion     : servo torque enable               */
-/* NAME         : torque                            */
+/* Funcyion     : Servo torque enable               */
+/* NAME         : Torque                            */
 /* Argument     : ID (Servo ID)                     */
 /*              : data (Torque enable)              */
 /* Return value : ---                               */
 /*--------------------------------------------------*/
-void torque (unsigned char ID, unsigned char data){
+void Torque (unsigned char ID, unsigned char data){
 
     unsigned char TxData[9];    // TransmitByteData [9byte]
     unsigned char CheckSum = 0; // CheckSum calculation
@@ -56,13 +56,14 @@ void torque (unsigned char ID, unsigned char data){
     REDE = 0;                   // RS485 Transmitt disable
 }
 /*--------------------------------------------------*/
-/* Function     : servo goal position instruction   */
-/* NAME         : GoalPosition                      */
+/* Function     : Servo goal position instruction   */
+/*                (Not specify time)                */
+/* NAME         : SetPosition                       */
 /* Argument     : ID (Servo ID)                     */
-/*              : data (Goal Position)              */
+/*              : data (Set position)              */
 /* Return value : ---                               */
 /*--------------------------------------------------*/
-void GoalPosition (unsigned char ID, int data){
+void SetPosition (unsigned char ID, short data){
 
     unsigned char TxData[10];   // TransmitByteData [10byte]
     unsigned char CheckSum = 0; // CheckSum calculation
@@ -79,7 +80,7 @@ void GoalPosition (unsigned char ID, int data){
     TxData[8] = (unsigned char)0x00FF & (data >> 8);    // Hi  byte
     
     // CheckSum calculation
-    for(int i=2; i<=8; i++){
+    for(int i=3; i<=8; i++){
         CheckSum = CheckSum ^ TxData[i];                // XOR from ID to Data
     }
     TxData[9] = CheckSum;       // Sum
@@ -91,6 +92,47 @@ void GoalPosition (unsigned char ID, int data){
     wait_us(250);               // Wait for transmission
     REDE = 0;                   // RS485 Transmit disable
 }
+
+/*--------------------------------------------------*/
+/* Function     : Set the time and position         */ 
+/* NAME         : SetTimeAndPosition                */
+/* Argument     : ID (servo ID)                     */
+/*              : data (set position)               */
+/*              : time (time to move)               */ 
+/* Return value : ---                               */
+/*--------------------------------------------------*/
+void SetTimeAndPosition(unsigned char ID, short data, unsigned short stime){
+    unsigned char TxData[12];   // TransmitByteData [10byte]
+    unsigned char CheckSum = 0; // CheckSum calculation
+	
+    TxData[0] = 0xFA;           // Header
+    TxData[1] = 0xAF;           // Header
+    TxData[2] = ID;             // ID
+    TxData[3] = 0x00;           // Flags
+    TxData[4] = 0x1E;           // Address
+    TxData[5] = 0x04;           // Length
+    TxData[6] = 0x01;           // Count
+                                // Data
+    TxData[7] = (unsigned char)0x00FF & data;           // Low byte
+    TxData[8] = (unsigned char)0x00FF & (data >> 8);    // Hi  byte
+    TxData[9] = (unsigned char)0x00FF & stime;           // Low byte
+    TxData[10] = (unsigned char)0x00FF * (stime >> 8);   // Hi  byte
+    
+    // CheckSum calculation
+    for (int i=3; i<=10; i++) {
+        CheckSum = CheckSum ^ TxData[i];
+    }
+    TxData[11] = CheckSum;      // Sum
+
+    // Send Packet
+    REDE = 1;                   // Transmit Enable
+    for(int i=0; i<=11; i++){
+        device.putc(data[i]);
+    }
+    wait_us(250);               // Wait for transmission
+    REDE = 0;                   // Transmitt disable
+}
+
 /*--------------------------------------------------*/
 /* Function     : main program                      */ 
 /* NAME         : main                              */
@@ -98,14 +140,19 @@ void GoalPosition (unsigned char ID, int data){
 /* Return value : ---                               */
 /*--------------------------------------------------*/
 int main() {
-    init();                     // initialize
-    torque(0x01, 0x01);         // ID = 1(0x01) , torque = OFF (0x00)
+    Init();                     // initialize
+    Torque(0x01, 0x01);         // ID = 1(0x01) , torque = OFF (0x00)
                                 // torque = OFF(0x00), ON(0x01), BRAKE(0x02)
     wait(1);                    // wait (1sec)
     while(1){
-        GoalPosition(0x01,300); // ID = 1(0x01) , GoalPosition = 30.0deg(300)
+        SetPosition(0x01,300); // ID = 1(0x01) , GoalPosition = 30.0deg(300)
         wait(1);                // wait (1sec)
-        GoalPosition(0x01,-300);// ID = 1(0x01) , GoalPosition = -30.0deg(-300) 
+        SetPosition(0x01,-300);// ID = 1(0x01) , GoalPosition = -30.0deg(-300) 
         wait(1);                // wait (1sec)
+
+        SetTimeAndPosition(0x01, 500, 35);
+	wait(1);
+	SetTimeAndPosition(0x01, -500, 35);
+	wait(1);
     }
 }
